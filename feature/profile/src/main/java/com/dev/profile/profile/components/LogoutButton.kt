4ec5f61ae6b5179f8e661978a0c117926e05285e
mohsen <mohamedm.abdlnabi@gmail.com>
2026-05-20 02:56:50 +0300

    style(profile): update border widths to 1.dp
    - Replace the use of `MaterialTheme.spacing.xxs` with a fixed `1.dp` for borders in `LogoutButton` and `TopSection`.
    - Ensures visual consistency for component outlines by using a dedicated stroke width instead of a spacing token.

--- feature/profile/src/main/java/com/dev/profile/profile/components/LogoutButton.kt
+++ feature/profile/src/main/java/com/dev/profile/profile/components/LogoutButton.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.designsystem.theme.TravioTheme
 import com.example.designsystem.theme.spacing
 import com.example.feature.profile.R
@@ -35,7 +36,7 @@ fun LogoutButton(
         colors = ButtonDefaults.outlinedButtonColors(
             contentColor = MaterialTheme.colorScheme.onBackground
         ),
-        border = BorderStroke(width = MaterialTheme.spacing.xxs, color = MaterialTheme.colorScheme.error)
+        border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.error)
     ) {
         Row(
             horizontalArrangement = Arrangement.Center,
